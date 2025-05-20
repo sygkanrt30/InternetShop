188b67dd63b98837c6ef90029c053a0ea58e5f10
Yanin Viacheslav <slava.vy.2006@gmail.com>
2025-05-20 10:02:02 +0300

    change start page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Register from "./component/register/Register";
 import Authorization from "./component/authorization/Authorization"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
 import MainPage from "./component/main_page/MainPage";
 import {AuthProvider} from './component/security/AuthContext';
 import ProtectedRoute from './component/security/ProtectedRoute';
@@ -12,6 +12,7 @@ function App() {
         <AuthProvider>
             <Router>
                 <Routes>
+                    <Route path="/" element={<Navigate to="/login" replace />} />
                     <Route path="/login" element={<Authorization />} />
                     <Route path="/signup" element={<Register />} />
                     <Route path="/internet-shop" element={<ProtectedRoute><MainPage /></ProtectedRoute>}/>
